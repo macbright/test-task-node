8f2df8c27af57d0c0fa5de61b84ab801ff2e0edd
bright okike <brightokike@brights-MacBook-Air.local>
2023-07-14 09:08:46 +0300

    test task

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,7 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { EventModule } from './event/event.module';
 
+
 @Module({
   imports: [EventModule],
   controllers: [AppController],
